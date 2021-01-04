029ed9c7c621ea8434fb71fa62b247eee222cbc0
Farzan Ahmad <farzanahmad863@yahoo.com>
2021-01-04 14:07:22 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assets_Management_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
