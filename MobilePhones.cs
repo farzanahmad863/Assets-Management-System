642b33b691297648175997522a883e9cfbd71414
Farzan Ahmad <farzanahmad863@yahoo.com>
2021-01-05 19:38:16 +0100

    Added classes of LaptopComputer and MobilePhones

---
+++ MobilePhones.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assets_Management_System
+{
+    class MobilePhones
+    {
+        public MobilePhones()
+        {
+
+        }
+    }
+}
