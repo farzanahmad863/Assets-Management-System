1559d78c96a386bbc7fafd199b63b29e0b932a76
Farzan Ahmad <farzanahmad863@yahoo.com>
2021-01-07 19:10:20 +0100

    Fixed linq query
    Query now works as it should. Groups the list of assets into laptops and phones and orders them according to ascending price.

--- Asset.cs
+++ Asset.cs
@@ -4,12 +4,15 @@ using System.Text;
 
 namespace Assets_Management_System
 {
-    abstract class Asset
+    class Asset
     {
         public DateTime PurchaseDate { get; set; }
         public int Price { get; set; }
         public string ModelName { get; set; }
-
+        public Asset()
+        {
+            
+        }
         public void AssetCatalog() { }
 
 
--- Program.cs
+++ Program.cs
@@ -10,31 +10,23 @@ namespace Assets_Management_System
             //Console.WriteLine("Hello World!");  
             //Console.WriteLine(MacBook.PurchaseDate);
             List<Asset> CompanyAssets = new List<Asset>();
-            CompanyAssets.Add(new LaptopComputer(DateTime.Today, 1200, "Macbook Pro"));
+            CompanyAssets.Add(new LaptopComputer(DateTime.Today, 1400, "Macbook Pro"));
             CompanyAssets.Add(new LaptopComputer(DateTime.Today, 1300, "Asus Vivobook"));
-            CompanyAssets.Add(new LaptopComputer(DateTime.Today, 1100, "Lenovo Ideapad"));
+            CompanyAssets.Add(new LaptopComputer(DateTime.Today, 1600, "Lenovo Ideapad"));
             CompanyAssets.Add(new MobilePhone(DateTime.Today, 1800, "Iphone 12"));
             CompanyAssets.Add(new MobilePhone(DateTime.Today, 1000, "Samsung Galaxy s20"));
             CompanyAssets.Add(new MobilePhone(DateTime.Today, 1700, "Nokia 5.8"));
-            
-            foreach (Asset asset in CompanyAssets)
+            var query = CompanyAssets.GroupBy(asset => asset.GetType())
+                  .Select(group =>group.OrderBy(x => x.Price));
+
+            foreach (var group in query)
             {
-                if (asset is LaptopComputer)
+                
+                foreach (var asset in group)
                 {
-                    
-                    Console.WriteLine(asset.ModelName.PadRight(20) + asset.Price.ToString().PadRight(20) + asset.PurchaseDate.ToString("dddd, dd MMMM yyyy"));
+                  Console.WriteLine(asset.ModelName.PadRight(20) + asset.Price.ToString().PadRight(20) + asset.PurchaseDate.ToString("dddd, dd MMMM yyyy"));
                 }
-                else if (asset is MobilePhone)
-                {
-                    Console.WriteLine(asset.ModelName.PadRight(20) + asset.Price.ToString().PadRight(20) + asset.PurchaseDate.ToString("dddd, dd MMMM yyyy"));
-                }
-
             }
-
-
-
-
-
 
 
 
